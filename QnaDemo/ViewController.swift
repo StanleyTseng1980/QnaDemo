500e07d173018c4a4ff7e01d5c7eebe9a578a198
stanleytseng <stanley690316@gmail.com>
2019-10-18 14:56:53 +0800

    Initial Commit

---
+++ QnaDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QnaDemo
+//
+//  Created by Stanley Tseng on 2019/10/18.
+//  Copyright © 2019 StanleyAppWorld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
